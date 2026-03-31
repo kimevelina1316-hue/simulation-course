da1bc7519e82a19df8a2e3ab3c2a7eb74912d513
evkim <kimevelina1316@gmail.com>
2026-04-01 02:01:42 +0700

    Add files via upload

---
+++ lab05/Случайныесобытия/Случайныесобытия/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Случайныесобытия
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.textBoxQuestion = new System.Windows.Forms.TextBox();
+            this.buttonAnswer = new System.Windows.Forms.Button();
+            this.labelAnswer = new System.Windows.Forms.Label();
+            this.textBoxAnswer = new System.Windows.Forms.TextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(74, 60);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(150, 20);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "ЗаДаЙтЕ ВоПрОс:";
+            // 
+            // textBoxQuestion
+            // 
+            this.textBoxQuestion.Location = new System.Drawing.Point(253, 57);
+            this.textBoxQuestion.Name = "textBoxQuestion";
+            this.textBoxQuestion.Size = new System.Drawing.Size(387, 26);
+            this.textBoxQuestion.TabIndex = 1;
+            // 
+            // buttonAnswer
+            // 
+            this.buttonAnswer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.buttonAnswer.Location = new System.Drawing.Point(253, 143);
+            this.buttonAnswer.Name = "buttonAnswer";
+            this.buttonAnswer.Size = new System.Drawing.Size(182, 38);
+            this.buttonAnswer.TabIndex = 2;
+            this.buttonAnswer.Text = "Get answer!";
+            this.buttonAnswer.UseVisualStyleBackColor = false;
+            this.buttonAnswer.Click += new System.EventHandler(this.buttonAnswer_Click);
+            // 
+            // labelAnswer
+            // 
+            this.labelAnswer.AutoSize = true;
+            this.labelAnswer.Location = new System.Drawing.Point(125, 245);
+            this.labelAnswer.Name = "labelAnswer";
+            this.labelAnswer.Size = new System.Drawing.Size(63, 20);
+            this.labelAnswer.TabIndex = 3;
+            this.labelAnswer.Text = "ОтВеТ:";
+            // 
+            // textBoxAnswer
+            // 
+            this.textBoxAnswer.Location = new System.Drawing.Point(253, 242);
+            this.textBoxAnswer.Name = "textBoxAnswer";
+            this.textBoxAnswer.ReadOnly = true;
+            this.textBoxAnswer.Size = new System.Drawing.Size(283, 26);
+            this.textBoxAnswer.TabIndex = 4;
+            this.textBoxAnswer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.Color.Khaki;
+            this.buttonClear.Location = new System.Drawing.Point(253, 328);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(182, 37);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "Clear!";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(716, 417);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.textBoxAnswer);
+            this.Controls.Add(this.labelAnswer);
+            this.Controls.Add(this.buttonAnswer);
+            this.Controls.Add(this.textBoxQuestion);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.TextBox textBoxQuestion;
+        private System.Windows.Forms.Button buttonAnswer;
+        private System.Windows.Forms.Label labelAnswer;
+        private System.Windows.Forms.TextBox textBoxAnswer;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ lab05/Случайныесобытия/Случайныесобытия/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Случайныесобытия
+{
+    public partial class Form1 : Form
+    {
+        
+        private const double ProbabilityYes = 0.5;
+
+        
+        private static readonly BigInteger M = BigInteger.Pow(2, 63);
+        private static readonly BigInteger Beta = BigInteger.Pow(2, 32) + 3;
+        private static BigInteger xStar;
+
+      
+        private const int WarmupIterations = 100; // Количество итераций прогрева
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 1. Инициализация генератора
+            InitializeGenerator(42);
+
+            // 2.  ПРОГРЕВ ГЕНЕРАТОРА
+            WarmupGenerator();
+
+            // 3. Настройки интерфейса
+            textBoxAnswer.BackColor = Color.White;
+            textBoxAnswer.Text = "";
+            textBoxQuestion.Focus();
+
+            // 4. Информацию о прогреве в консоль
+            Console.WriteLine($"[INFO] Генератор прогрет: {WarmupIterations} итераций");
+        }
+
+        /// <summary>
+        /// базовый датчик: генерирует число α ∈ [0, 1)
+        /// </summary>
+        private double GenerateAlpha()
+        {
+            xStar = (Beta * xStar) % M;
+            double alpha = (double)xStar / (double)M;
+            return alpha;
+        }
+
+        /// <summary>
+        /// Инициализация генератора (зерно)
+        /// </summary>
+        private void InitializeGenerator(int seed)
+        {
+            xStar = Beta + seed;
+            Console.WriteLine($"[INIT] Генератор инициализирован: seed = {seed}");
+        }
+
+        /// <summary>
+        /// ПРОГРЕВ ГЕНЕРАТОРА
+        /// Генерируем и отбрасываем N чисел для "перемешивания" состояния
+        /// </summary>
+        private void WarmupGenerator()
+        {
+            Console.WriteLine($"[WARMUP] Начинаю прогрев: {WarmupIterations} итераций...");
+
+            for (int i = 0; i < WarmupIterations; i++)
+            {
+                // Генерируем число, но НЕ используем его
+                xStar = (Beta * xStar) % M;
+            }
+
+            Console.WriteLine($"[WARMUP] Прогрев завершён!");
+        }
+
+        
+        private void buttonAnswer_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxQuestion.Text))
+            {
+                MessageBox.Show("Пожалуйста, введите вопрос!", "Внимание",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxQuestion.Focus();
+                return;
+            }
+
+            // === АЛГОРИТМ ИЗ ПРЕЗЕНТАЦИИ + ВАШ ГЕНЕРАТОР ===
+            double alpha = GenerateAlpha();
+            string result = (alpha < ProbabilityYes) ? "ДА" : "НЕТ";
+
+            // Показываем ответ
+            textBoxAnswer.Text = result;
+
+            // Визуальная подсветка
+            if (result == "ДА")
+            {
+                textBoxAnswer.BackColor = Color.LightGreen;
+                textBoxAnswer.ForeColor = Color.DarkGreen;
+            }
+            else
+            {
+                textBoxAnswer.BackColor = Color.LightCoral;
+                textBoxAnswer.ForeColor = Color.DarkRed;
+            }
+
+            // Отладка в Output (View → Output)
+            Console.WriteLine($"[DEBUG] α = {alpha:F10}, результат = {result}");
+        }
+
+       
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxQuestion.Clear();
+            textBoxAnswer.Clear();
+            textBoxAnswer.BackColor = Color.White;
+            textBoxAnswer.ForeColor = Color.Black;
+            textBoxQuestion.Focus();
+        }
+    }
+}
